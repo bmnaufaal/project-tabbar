28aa3167b0fc8a83c604e86b35f049c8b36e9beb
gorbyno <gorbynositepuu@gmail.com>
2022-06-14 13:39:57 +0700

    halaman register

---
+++ project-tabbar/SignUpController.swift
@@ -0,0 +1,15 @@
+//
+//  SignUpController.swift
+//  project-tabbar
+//
+//  Created by Gorbyno S on 14/06/22.
+//
+
+import Foundation
+
+import UIKit
+
+
+class SignUpController: UIViewController {
+    
+}
--- project-tabbar/ViewController.swift
+++ project-tabbar/ViewController.swift
@@ -9,6 +9,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func SignUp(_ sender: Any) {
+        let a = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "SignUpController")
+        self.navigationController?.present(a, animated: true)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
